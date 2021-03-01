f0ffe169d99d71c5599674232387549ad1d524d9
Trevor Pilgrim <pilg5971@stthomas.edu>
2021-02-28 22:12:18 -0600

    Worked on Code

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -9,6 +9,7 @@
 void splitString(std::string text, char d, std::vector<std::string>& result);
 void vectorOfStringsToArrayOfCharArrays(std::vector<std::string>& list, char ***result);
 void freeArrayOfCharArrays(char **array, size_t array_length);
+void recordCommand(std::string command, std::vector<std::string>& history, int* counter); 
 
 int main (int argc, char **argv)
 {
@@ -22,11 +23,13 @@ int main (int argc, char **argv)
      *   Example code - remove in actual program                                        *
      ************************************************************************************/
     // Shows how to loop over the directories in the PATH environment variable
+    
     int i;
     for (i = 0; i < os_path_list.size(); i++)
     {
         printf("PATH[%2d]: %s\n", i, os_path_list[i].c_str());
     }
+    
     /************************************************************************************
      *   End example code                                                               *
      ************************************************************************************/
@@ -37,6 +40,10 @@ int main (int argc, char **argv)
 
     std::vector<std::string> command_list; // to store command user types in, split into its variour parameters
     char **command_list_exec; // command_list converted to an array of character arrays
+    char **command_history; 
+    int counter = 0;
+
+
     // Repeat:
     //  Print prompt for user input: "osshell> " (no newline)
     //  Get user input for next command
@@ -46,10 +53,91 @@ int main (int argc, char **argv)
     //   If yes, execute it
     //   If no, print error statement: "<command_name>: Error command not found" (do include newline)
 
+    bool running = true; 
+    while (running) {
+    	/*
+    	Recommended using the cis/stat library when checking if a file is available
+    	this will allow `struct stat;` to query file vs dir, rwx perms, etc (linux specific)
+    	Or std::filesystem
+    	But this is 2017 std, and makefile is setting to 2011 (-std=c++11); would need to edit
+    	Takes in string, can check "does this exist?" and if exists, "exec perms?"
+    	*/
+    	
+    	//Make command var
+    	std::string command; 
+    	char* command_parts; 
+    	
+    	printf("Made command var\n"); 
+    	
+    	//Prompt and retrieve user input
+    	printf("osshell> "); 
+		std::getline(std::cin, command); 
+		
+		
+    	//TODO use the splitString() method given by teacher, it's a delimiter splitter
+    	//gives back a null-terminated list of strings in given char**
+    	
+    	printf("Prompted, recieved input\n"); 
 
+    	
+    	//splitString(command, ' ', &command_parts); 
+    	
+    	//printf("Split the string\n"); 
+    	
+    	std::cout << "[" << command << "]" << std::endl; 
+        printf("Before Counter increment\n");
+        counter = counter + 1;
+        printf("Before recordCommand\n");
+    	
+        //recordCommand(command, &command_history, &counter); //TODO make this method, or write it here
+    	printf("After recordCommand\n");
+        
+    	if(command == "exit") {
+    		printf("exiting..."); 
+    		running = false; 
+    	} else if (command == "") {
+    		//Do nothing, reprompt
+    	} else if (command == "history") { //TODO split stringing
+    		//TODO print (or clear) history up to 128 (or X)
+    		
+    		//printing command_list
+    		
+    		
+    	} else {
+            continue;
+            /*
+    		//TODO search for executable; PATH, or ., or /
+            int i;
+            for (i = 0; i < os_path_list.size(); i++)
+            {
+                if(command == os_path_list[i].c_str());{
+                    
+                    int pid = fork();
+
+                    //Child Process (Thread)
+                    if (pid == 0){
+                        //Execute program
+                    }
+
+
+                }
+                //printf("PATH[%2d]: %s\n", i, os_path_list[i].c_str());
+            }
+
+
+    		//Remember to thread this, carefully
+            */
+    	}
+    	
+    	printf("Made it through one loop\n"); 
+    	
+    }
+
+    
     /************************************************************************************
      *   Example code - remove in actual program                                        *
      ************************************************************************************/
+    /*
     // Shows how to split a command and prepare for the execv() function
     std::string example_command = "ls -lh";
     splitString(example_command, ' ', command_list);
@@ -79,10 +167,10 @@ int main (int argc, char **argv)
     // free memory for `command_list_exec`
     freeArrayOfCharArrays(command_list_exec, command_list.size() + 1);
     printf("------\n");
+    */
     /************************************************************************************
      *   End example code                                                               *
      ************************************************************************************/
-
 
     return 0;
 }
@@ -180,4 +268,18 @@ void freeArrayOfCharArrays(char **array, size_t array_length)
         }
     }
     delete[] array;
+}
+
+/*
+    string: most recent command entered
+    string*: list of past 128 commands
+*/
+void recordCommand(std::string command, std::vector<std::string>& history, int* counter){
+	
+	strcpy(*(history)[*counter - 1], command.c_str()); 
+	
+	//TODO in the printHistory, check if null, then loop until
+	if(*(counter) == 128){
+        *(counter) = 0;
+    } 
 }
